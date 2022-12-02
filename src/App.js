0391c18ca8ce9ce24350b9c22f6b8b6bc0c9d3d6
cutekitty415 <115032243+cutekitty415@users.noreply.github.com>
2022-12-02 15:17:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,8 +18,13 @@ function App() {
     setCartItems([...cartItems, {...item, qty: 1}]);
   }
 
-  const onDeleteItem = (item) => {
+  const 
+  Item = (item) => {
     setCartItems(cartItems.filter((x) => x.name !== item.name));
+  }
+  
+  const emptyCart = (item) => {
+    setCartItems([]);
   }
 
   const [type, setType] = useState("All");
@@ -133,7 +138,7 @@ function App() {
 
             <div className="col-12">
               <h4>
-                <Cart onAddItem={onAddItem} onDeleteItem={onDeleteItem} cartItems={cartItems} itemsPrice={itemsPrice}></Cart>
+                <Cart cartItems={cartItems} emptyCart={emptyCart} itemsPrice={itemsPrice}></Cart>
               </h4>
             </div>  
 
