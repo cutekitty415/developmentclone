e484b73e013daaed98a5bb964d3a3d74ddf11d8a
cutekitty415 <emily_y_wang@brown.edu>
2022-12-02 04:38:32 -0500

    yee

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import ProductItem from "./components/ProductItem.js";
 import Cart from "./components/Cart.js"
 import { ListGroup } from "react-bootstrap";
 import { Nav } from 'react-bootstrap';
+import FilterButton from './components/FilterButton';
+import { createPortal } from 'react-dom';
 
 function App() {
 
-  const [type, setType] = useState("All");
 
   const [cartItems, setCartItems] = useState([]);
   const itemsPrice = cartItems.reduce((a,c) => a + c.price, 0);
@@ -23,20 +24,17 @@ function App() {
     setCartItems(cartItems.filter((x) => x.name !== item.name));
   }
 
+  const [type, setType] = useState("All");
+
   const selectFilterType = eventKey => {
     setType(eventKey);
   };
-
 
   const matchesFilterType = item => {
     // all items should be shown when no filter is selected
     if(type === "All") { 
       return true
     } else if (type === item.brand) {
-      return true
-    }else if (type === item.color) {
-      return true
-    }else if (type === item.color) {
       return true
     } else {
       return false
@@ -45,6 +43,24 @@ function App() {
 
   const filteredData = productData.filter(matchesFilterType);
 
+  const [color, setColor] = useState("All");
+
+  const selectFilterColor = eventKey => {
+    setColor(eventKey);
+  };
+
+  const matchesFilterColor = item => {
+    // all items should be shown when no filter is selected
+    if(color === "All") { 
+      return true;
+    } else if (color === item.color) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const filteredDataColor = filteredData.filter(matchesFilterColor);
 
 
   const [sort, setSort] = useState("Unsorted");
@@ -61,7 +77,7 @@ function App() {
     }
   }
 
-  const sortedData = matchesSort(filteredData);
+  const sortedData = matchesSort(filteredDataColor);
 
 
   
@@ -86,30 +102,32 @@ function App() {
             </div>
 
             <div class="col-sm-4"> 
-            <div className="col-8">
+            <div className="col-12">
               <h4>
                 Favorites Price:
                 <Cart onAddItem={onAddItem} onDeleteItem={onDeleteItem} cartItems={cartItems} itemsPrice={itemsPrice}></Cart>
               </h4>
             </div>  
-            <div className="col-8">
+            <div className="col-12">
               <h4>
                 Filters
               </h4>
               <Nav onSelect={selectFilterType}>
-              <Nav.Item><Nav.Link eventKey="All">All</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Alphalete">Alphalete</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Alo Yoga">Alo Yoga</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Lululemon">Lululemon</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Black">Black</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Blue">Blue</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="Pink">Pink</Nav.Link></Nav.Item>
+              <FilterButton selectFilterType={selectFilterType} eventKey={"All"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterType} eventKey={"Alphalete"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterType} eventKey={"Alo Yoga"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterType} eventKey={"Lululemon"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterType} eventKey={"Nike"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterColor} eventKey={"Black"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterColor} eventKey={"Blue"}></FilterButton>
+              <FilterButton selectFilterType={selectFilterColor} eventKey={"Pink"}></FilterButton>
+
               </Nav>
-              <h6>
+              <h4>
                 Sort 
-              </h6>
+              </h4>
               <Nav onSelect={selectSort}>
-              <Nav.Item><Nav.Link eventKey="Unsorted">Unsorted</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link eventKey="Unsorted">Popular</Nav.Link></Nav.Item>
               <Nav.Item><Nav.Link eventKey="Sorted">Sort by Price</Nav.Link></Nav.Item>
 
               </Nav>
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,30 +1,32 @@
 import React from "react";
-import Data from "./assets/product-data.json";
- 
-const Buttons = ({setCartItems, brandItems, filterItem}) => {
+import Button from 'react-bootstrap/Button';
+import {useState} from "react";
+
+function FilterButton(props) {
+
+  const {selectFilterType, eventKey} = props;
+
+  const [pressed, setPressed] = useState(false);
+  const [variant, setVariant] = useState("success");
+
+  function handleClick(eventKey, pressed) {
+    if (pressed==false){
+      selectFilterType(eventKey);
+      setVariant("danger");
+      setPressed(true);
+    } else {
+      selectFilterType("All");
+      setVariant("success");
+      setPressed(false);
+    }
+  }
+
   return (
-    <>
-      <div className="d-flex justify-content-center">
-        {brandItems.map((item, name) => {
-          return (
-            <button
-              className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
-              onClick={() => filterItem(item)}
-              key={name}
-            >
-              {item}
-            </button>
-          );
-        })}
-        <button
-          className="btn-dark text-white p-1 px-3 mx-5 fw-bold btn"
-          onClick={() => setCartItems(Data)}
-        >
-          All
-        </button>
-       </div>
-    </>
+    <div className="d-flex justify-content-center">
+      <Button variant={variant} onClick={() => handleClick(eventKey, pressed)}>{eventKey}</Button>
+    </div>
   );
+
 };
  
-export default Buttons;
+export default FilterButton;
